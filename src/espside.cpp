97988551d4eaafe63b1571c5390ecc98bc6d9fac
0xTunay <tigrus232323k@gmail.com>
2025-05-17 22:25:34 +0300

    add struct esp

--- src/espside.cpp
+++ src/espside.cpp
@@ -2,7 +2,23 @@
 // Created by Tunay on 16.05.25.
 //
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
+
+const char *ssid = "";
+const char *password = "";
+
+WifiServer server = WifiServer(80);
+
 void setup() {
+    Serial.begin(9600);
+    Wifi.begin(ssid,password);
+    if (Wifi.status != WL_CONNECTED) {
+        delay(500);
+        Serial.println("Connection connect..");
+    }
+    Serial.print("Connected to Wifi ");
+    server.begin();
+
     // write your initialization code here
 }
 
