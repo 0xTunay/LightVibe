bfd9277767c4a2332539818174016fa13e9cfb10
0xTunay <tigrus232323k@gmail.com>
2025-05-18 10:54:04 +0300

    change code

--- src/espside.cpp
+++ src/espside.cpp
@@ -1,27 +1,27 @@
-//
-// Created by Tunay on 16.05.25.
-//
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 
-const char *ssid = "";
-const char *password = "";
 
-WifiServer server = WifiServer(80);
+const char* ssid = "";
+const char* password = "";
 
 void setup() {
-    Serial.begin(9600);
-    Wifi.begin(ssid,password);
-    if (Wifi.status != WL_CONNECTED) {
-        delay(500);
-        Serial.println("Connection connect..");
-    }
-    Serial.print("Connected to Wifi ");
-    server.begin();
+    Serial.begin(115200);
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
 
-    // write your initialization code here
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
 }
 
 void loop() {
-    // write your code here
 }
